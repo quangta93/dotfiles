16ce35ecfb0e0d5d49f8348ddd5d6689a9923693
Quang Tran <quangta93@MBP.local>
2019-08-27 22:57:50 -0500

    update brew script

--- brew.sh
+++ brew.sh
@@ -1,14 +1,14 @@
+#!/bin/bash
 
 # Before running this script:
-# Install Xcode from App Store
-
-# homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# oh-my-zsh
+# Install latest Xcode from App Store
 sudo xcodebuild -license
 
-# setup zsh
+## Homebrew
+echo 'Installing homebrew package manager'
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo 'Loading packages for oh-my-zsh'
 # powerlevel9k
 brew tap sambadevi/powerlevel9k
 brew install powerlevel9k
@@ -22,14 +22,36 @@ brew cask install font-hack-nerd-font
 mkdir -p $HOME/.zsh/plugins/bd
 curl https://raw.githubusercontent.com/Tarrasch/zsh-bd/master/bd.zsh > $HOME/.zsh/plugins/bd/bd.zsh
 
-# personal dotfiles
-mkdir $HOME/Development
-cd $HOME/Development
-git clone https://https://github.com/quangta93/dotfiles.git
-
+echo 'Installing oh-my-zsh shell'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Personal dotfiles
+echo 'Downloading personal dotfiles'
+mkdir $HOME/Development
+# TODO: test clone
+git clone https://github.com/quangta93/dotfiles.git $HOME/Development/dotfiles
+
+echo 'Loading zsh and vim configurations'
 rm $HOME/.zshrc
 rm $HOME/.vimrc
 ln -s $HOME/Development/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/Development/dotfiles/.vimrc $HOME/.vimrc
+
+## iTerm2
+echo 'Installing iTerm2'
+brew cask install iterm2
+
+## TODO: programmatically customize font & theme for iterm2
+
+## VS Code
+echo 'Installing Visual Studio Code and its extensions'
+brew tap caskroom/cask
+brew search visual-studio-code
+brew cask info visual-studio-code
+brew cask install visual-studio-code
+
+## TODO: VS Code Extensions
+
+echo 'Loading VS Code settings'
+
+brew cleanup
